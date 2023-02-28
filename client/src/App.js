78230d1fcabcf6ddcfe986179276baf64a802d55
nikolapivac <npivac00@fesb.hr>
2023-02-28 21:18:34 +0100

    Frontend (form for long URL and POST request)

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [longUrl, setLongUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+
+  const shorten = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:5001/api/url/shorten", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({longUrl})
+    });
+
+    const data = await response.json();
+    setShortUrl(data.shortUrl);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+
+      <h1 className="title">URL shortener</h1>
+      <form onSubmit={shorten}>
+        <label>Enter your URL here:</label>
+        <input type="text" value={longUrl} onChange={(e) => setLongUrl(e.target.value)} />
+        <button type="submit">SHORTEN</button>
+      </form>
+
+      {shortUrl && (
+        <div className="short">
+          Short URL: <a href={shortUrl}>{shortUrl}</a>
+        </div>
+      )}
+
     </div>
   );
 }
