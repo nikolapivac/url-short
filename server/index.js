a9971b590c40c7fee89859550f2e3d8f7e3be02b
nikolapivac <npivac00@fesb.hr>
2023-02-28 21:17:22 +0100

    Added permission for localhost:3000

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ const app = express();
 // Connect to the database 
 connectToDB();
 
+// allows the client to accept requests from localhost:3000
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+  
+
 // allows the server to accept json data
 app.use(express.json({ extended: false }));
 
